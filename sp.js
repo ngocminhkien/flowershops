a5b09a16c78eac86fb1d3f0ed3a1fed7f8998774
ngocminhkien <160562512+ngocminhkien@users.noreply.github.com>
2024-07-12 13:13:11 +0700

    flowershopp

---
+++ sp.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.sp1').hover(function() {
+        $(this).css("transform", "scale(1.2)");
+    }, function() {
+        $(this).css("transform", "scale(1)");
+    });
+  });
+  
+  $(document).ready(function(){
+    $('.sp2').hover(function() {
+        $(this).css("transform", "scale(1.2)");
+    }, function() {
+        $(this).css("transform", "scale(1)");
+    });
+  });
+  
+  $(document).ready(function(){
+    $('.sp3').hover(function() {
+        $(this).css("transform", "scale(1.2)");
+    }, function() {
+        $(this).css("transform", "scale(1)");
+    });
+  });
+  
+  $(document).ready(function(){
+    $('.sp4').hover(function() {
+        $(this).css("transform", "scale(1.2)");
+    }, function() {
+        $(this).css("transform", "scale(1)");
+    });
+  });
+  
